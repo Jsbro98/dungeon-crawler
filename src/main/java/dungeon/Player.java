d9489384462a20d1e379890f7b0f99f2a8e59dd2
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-28 15:16:13 -0600

    cleaned up Player class

--- src/main/java/dungeon/Player.java
+++ src/main/java/dungeon/Player.java
@@ -1,29 +1,22 @@
 package dungeon;
 
 public class Player extends Entity {
-  private int health;
-
-  public Player() {
-    this.health = 100;
-  }
-
-
-  public void setHealth(int health) {
-    this.health = health;
+  public Player(int health) {
+    super(health);
   }
 
   @Override
   void heal(int healingValue) {
-    this.health = Math.max(100, healingValue + this.health);
+    setHealth(Math.max(100, healingValue + getHealth()));
   }
 
   @Override
   void takeDamage(int damageValue) {
-    this.health = Math.max(0, this.health - damageValue);
+    setHealth(Math.max(0, getHealth() - damageValue));
   }
 
   @Override
   public String toString() {
-    return "Player stats: health-(" + this.health + ")";
+    return "Player stats: health-(" + getHealth() + ")";
   }
 }
