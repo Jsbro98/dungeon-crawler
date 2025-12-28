f6397c9b26929bafd2badbe3cb7cf732f215e10f
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-28 15:45:21 -0600

    added new methods to TextRenderer

--- src/main/java/dungeon/ui/TextRenderer.java
+++ src/main/java/dungeon/ui/TextRenderer.java
@@ -1,7 +1,20 @@
 package dungeon.ui;
 
+import dungeon.Entity;
+
 public class TextRenderer {
+  private TextRenderer() {}
+
   public static void greetPlayer() {
     System.out.println("Welcome Player!");
+  }
+
+  public static void displayEntityStats(Entity entity) {
+    System.out.println("Health: " + entity.getHealth());
+  }
+
+  public static void clearScreen() {
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
   }
 }
