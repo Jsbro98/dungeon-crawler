309ee008f7d1dfb35b3f52ad5cf2495f6fd6ed74
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-28 16:52:24 -0600

    added basic Player inventory Map. Might change

--- src/main/java/dungeon/Player.java
+++ src/main/java/dungeon/Player.java
@@ -1,8 +1,15 @@
 package dungeon;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Player extends Entity {
+  // maybe create Inventory class?
+  private final Map<String, Item> inventory;
+
   public Player(int health) {
     super(health);
+    this.inventory = new HashMap<>();
   }
 
   @Override
