23c50010e3eb66c82a1f54fcf3cedd73d7b9972f
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-28 15:15:53 -0600

    adjusted Entity base class. provided base utility

--- src/main/java/dungeon/Entity.java
+++ src/main/java/dungeon/Entity.java
@@ -3,10 +3,19 @@ package dungeon;
 public abstract class Entity {
   private int health;
 
+  protected Entity(int health) {
+    this.health = health;
+  }
+
   abstract void heal(int healingValue);
+
   abstract void takeDamage(int damageValue);
 
   public int getHealth() {
-    return this.health;
+    return health;
+  }
+
+  public void setHealth(int health) {
+    this.health = health;
   }
 }
