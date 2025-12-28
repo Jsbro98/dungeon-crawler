f73f1f30635c1f57bea9795d9a805a47a8e42b1f
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-28 15:23:22 -0600

    removed unneeded methods in Player

--- src/main/java/dungeon/Player.java
+++ src/main/java/dungeon/Player.java
@@ -6,16 +6,6 @@ public class Player extends Entity {
   }
 
   @Override
-  void heal(int healingValue) {
-    setHealth(Math.max(100, healingValue + getHealth()));
-  }
-
-  @Override
-  void takeDamage(int damageValue) {
-    setHealth(Math.max(0, getHealth() - damageValue));
-  }
-
-  @Override
   public String toString() {
     return "Player stats: health-(" + getHealth() + ")";
   }
