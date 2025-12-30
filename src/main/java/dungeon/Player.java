8d9e5d920f5f35d7ebd247f9a5544c3af898db12
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-29 20:16:38 -0600

    added Inventory delegation methods

--- src/main/java/dungeon/Player.java
+++ src/main/java/dungeon/Player.java
@@ -1,11 +1,25 @@
 package dungeon;
 
+import java.util.Map;
+
 public class Player extends Entity {
   private final Inventory inventory;
 
   public Player(int health) {
     super(health);
-    this.inventory = new Inventory(10);
+    this.inventory = new Inventory(2);
+  }
+
+  public void pickupItem(Item item) {
+    inventory.addToInventory(item.getName(), item);
+  }
+
+  public void dropItem(Item item) {
+    inventory.removeFromInventory(item.getName());
+  }
+
+  public Map<String, Item> viewInventory() {
+    return inventory.getInventory();
   }
 
   @Override
