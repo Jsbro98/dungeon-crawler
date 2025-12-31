4626db5f3f82df3866599fe18fd7975a167edb93
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-30 19:55:26 -0600

    implemented Attacker interface and added an equipment slot for Player

--- src/main/java/dungeon/Player.java
+++ src/main/java/dungeon/Player.java
@@ -1,13 +1,19 @@
 package dungeon;
 
 import java.util.Map;
+import java.util.Random;
 
-public class Player extends Entity {
+public class Player extends Entity implements Attacker {
   private final Inventory inventory;
+  private final int strength;
+  // possibly make a class/record for EquipmentSlot?
+  private Item equipped;
 
-  public Player(int health) {
+  public Player(int health, int strength) {
     super(health);
     this.inventory = new Inventory(2);
+    this.strength = strength;
+    this.equipped = Item.NOTHING;
   }
 
   public void pickupItem(Item item) {
@@ -22,8 +28,35 @@ public class Player extends Entity {
     return inventory.getInventory();
   }
 
+  public void equipItem(Item item) {
+    equipped = item;
+  }
+
+  public Item getEquiped() {
+    return equipped;
+  }
+
   @Override
   public String toString() {
     return "Player stats: health-(" + getHealth() + ")";
+  }
+
+  @Override
+  public int getDamage() {
+    Random random = new Random();
+    int maxVariance = 3;
+    int baseDamage = strength;
+    Item currentEquipped = getEquiped();
+
+    if (currentEquipped != Item.NOTHING) {
+      baseDamage += currentEquipped.getPower();
+    }
+
+    return baseDamage + (random.nextInt(maxVariance));
+  }
+
+  @Override
+  public void attack(Entity other) {
+    other.takeDamage(this.getDamage());
   }
 }
