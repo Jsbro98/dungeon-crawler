9197b5182907f53b9a2412e1fa7ce8be776f8fd7
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-31 18:21:56 -0600

    Turn handles Room movement for the time being

--- src/main/java/dungeon/logic/Turn.java
+++ src/main/java/dungeon/logic/Turn.java
@@ -2,12 +2,27 @@ package dungeon.logic;
 
 import dungeon.Attacker;
 import dungeon.Entity;
+import dungeon.world.Room;
 
 public class Turn {
+
   public <T extends Entity & Attacker> void handleBattle(T one, T two) {
     while (one.isAlive() && two.isAlive()) {
       one.attack(two);
       two.attack(one);
+
+      IO.println(one);
+      IO.println(two);
     }
+  }
+
+  public Room moveRoom(Room from, String direction) {
+    Room to = from.getExit(direction);
+
+    if (to != null) {
+      return to;
+    }
+
+    return from;
   }
 }
