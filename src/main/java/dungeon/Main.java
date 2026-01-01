81265adb9759ac58b0db8b12efbcbb134901b5d5
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-31 18:22:14 -0600

    first real test case!

--- src/main/java/dungeon/Main.java
+++ src/main/java/dungeon/Main.java
@@ -1,7 +1,29 @@
 package dungeon;
 
+import dungeon.logic.Turn;
+import dungeon.world.Room;
+
 public class Main {
   static void main(String[] args) {
-    System.out.println("Hello, World!");
+    Room roomOne = new Room();
+    Room roomTwo = new Room();
+    Room currentRoom = roomOne;
+
+    roomOne.setExit("north", roomTwo);
+    roomTwo.setExit("south", roomOne);
+
+    Player player = new Player(100, 5);
+    Ogre ogre = new Ogre(200, 10);
+    roomTwo.addEntity(ogre);
+    roomOne.addEntity(player);
+
+    player.pickupItem(Item.SWORD);
+    player.equipItem(Item.SWORD);
+
+    Turn turn = new Turn();
+    currentRoom = turn.moveRoom(roomOne, "north");
+    if (currentRoom == roomTwo && roomTwo.containsEntity(ogre)) {
+      turn.handleBattle(player, ogre);
+    }
   }
 }
