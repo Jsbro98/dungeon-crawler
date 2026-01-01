3d1398277f6f89b33e01e316c42f269d580f39b2
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2025-12-31 18:57:30 -0600

    Turn takes Player as a parameter now

--- src/main/java/dungeon/logic/Turn.java
+++ src/main/java/dungeon/logic/Turn.java
@@ -2,17 +2,20 @@ package dungeon.logic;
 
 import dungeon.Attacker;
 import dungeon.Entity;
+import dungeon.Player;
 import dungeon.world.Room;
 
 public class Turn {
 
-  public <T extends Entity & Attacker> void handleBattle(T one, T two) {
-    while (one.isAlive() && two.isAlive()) {
-      one.attack(two);
-      two.attack(one);
+  public <T extends Entity & Attacker> void handleBattle(Player player, T opponent) {
+    IO.println("Player sees: " + opponent.getClass().getName());
+    IO.println("Player moves in to attack");
+    while (player.isAlive() && opponent.isAlive()) {
+      player.attack(opponent);
+      opponent.attack(player);
 
-      IO.println(one);
-      IO.println(two);
+      IO.println(opponent);
+      IO.println(player);
     }
   }
 
@@ -20,6 +23,7 @@ public class Turn {
     Room to = from.getExit(direction);
 
     if (to != null) {
+      System.out.println("Moving Rooms...");
       return to;
     }
 
