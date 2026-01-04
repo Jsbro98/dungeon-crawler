d8cd23beaa35197d0836d11c40830058b5280d66
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-04 16:11:38 -0600

    added helper method for getting commands

--- src/main/java/dungeon/ui/InputHandler.java
+++ src/main/java/dungeon/ui/InputHandler.java
@@ -5,11 +5,15 @@ import java.util.Scanner;
 public class InputHandler {
   private final Scanner userIn = new Scanner(System.in);
 
+  public String getCommand() {
+    IO.print("> ");
+    return userIn.nextLine().trim().toLowerCase();
+  }
+
   // TODO: add validation to return string
-  public String getDesiredMoveDirection() {
+  public String getMoveDirection() {
     IO.println("Which direction would you like to go?");
     TextRenderer.printDirections();
-
-    return userIn.nextLine();
+    return getCommand();
   }
 }
