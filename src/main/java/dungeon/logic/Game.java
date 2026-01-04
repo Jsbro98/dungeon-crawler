19e3ac1af232d3223f3ca69114dd21f417f69bd9
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-03 21:50:50 -0600

    added Turn and RoomRegistry to Game

--- src/main/java/dungeon/logic/Game.java
+++ src/main/java/dungeon/logic/Game.java
@@ -5,9 +5,13 @@ import dungeon.ui.TextRenderer;
 
 public class Game {
   private final Player player;
+  private final Turn turn;
+  private final RoomRegistry roomRegistry;
 
   public Game(Player player) {
     this.player = player;
+    this.turn = new Turn();
+    this.roomRegistry = new RoomRegistry();
   }
 
   public void startGame() {
