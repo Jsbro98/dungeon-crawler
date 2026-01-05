917b4c85da682bf13bae9e3d22903c30f18d5ac7
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-04 22:49:41 -0600

    updated test case in Main

--- src/main/java/dungeon/Main.java
+++ src/main/java/dungeon/Main.java
@@ -1,45 +1,20 @@
 package dungeon;
 
 import dungeon.logic.Game;
-import dungeon.logic.Turn;
-import dungeon.ui.TextRenderer;
 import dungeon.world.Room;
 
 public class Main {
   static void main(String[] args) {
     Game gameLoop = new Game(new Player(100, 10));
 
-    Room roomOne = new Room("A cold tundra with no trees for miles");
-    Room roomTwo = new Room("A dense forest with trees everywhere. creatures lurk in the fog...");
+    Room roomOne = new Room("a cold tundra with no trees for miles");
+    Room roomTwo = new Room("a dense forest with trees everywhere. creatures lurk in the fog...");
     roomOne.setExit("north", roomTwo);
     roomTwo.setExit("south", roomOne);
 
-    gameLoop.getRoomRegistry().addRoom(roomOne);
-    gameLoop.getRoomRegistry().addRoom(roomTwo);
-    gameLoop.initCurrentRoom();
+    gameLoop.addRoom(roomOne);
+    gameLoop.addRoom(roomTwo);
 
-
-    Player player = new Player(100, 10);
-    Ogre ogre = new Ogre(100, 10);
-    roomTwo.addEntity(ogre);
-    roomOne.addEntity(player);
-
-    TextRenderer.greetPlayer();
-    IO.print("Current Room: ");
-    TextRenderer.describe(roomOne);
-
-    player.pickupItem(Item.SWORD);
-    player.equipItem(Item.SWORD);
-
-    Turn turn = new Turn();
-    gameLoop.moveToRoom("north");
-    IO.print("Current Room: ");
-    TextRenderer.describe(gameLoop.getCurrentRoom());
-    if (gameLoop.getCurrentRoom() == roomTwo && roomTwo.containsEntity(ogre)) {
-      turn.handleBattle(player, ogre);
-    }
-
-    player.heal(25);
-    IO.println(player);
+    gameLoop.startGame();
   }
 }
