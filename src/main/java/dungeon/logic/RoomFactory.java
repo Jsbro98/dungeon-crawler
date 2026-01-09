1fa06b402831fed78b911a561aab98de0b3c3743
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-08 20:29:45 -0600

    refactored from double brace to List.of

--- src/main/java/dungeon/logic/RoomFactory.java
+++ src/main/java/dungeon/logic/RoomFactory.java
@@ -8,17 +8,16 @@ import java.util.Random;
 
 public class RoomFactory {
   private static final Random ROOM_RANDOM = new Random();
-  private static final List<String> ROOM_DESCRIPTIONS = new ArrayList<>() {{
-    add("a cold tundra with no trees for miles");
-    add("a dense forest with trees everywhere. creatures lurk in the fog...");
-    add("a urban settlement with tall spires and forgotten ruins");
-    add("a frozen lake with a treeline surrounding you");
-    add("a flat, volcanic plain at the base of the active mt. saxum");
-    add("a lush, sprawling jungle, where insects and greenery run rampant");
-    add("a canyon on unyielding awe. miles of chasms and formations litter the landscape");
-    add("an Ogre village with tribal elements scattered among the huts");
-    add("a cave with glowing cracks showing through the immense walls");
-  }};
+  private static final List<String> ROOM_DESCRIPTIONS = List.of(
+    "a cold tundra with no trees for miles",
+    "a dense forest with trees everywhere. creatures lurk in the fog...",
+    "a urban settlement with tall spires and forgotten ruins",
+    "a frozen lake with a treeline surrounding you",
+    "a flat, volcanic plain at the base of the active mt. saxum",
+    "a lush, sprawling jungle, where insects and greenery run rampant",
+    "a canyon on unyielding awe. miles of chasms and formations litter the landscape",
+    "an Ogre village with tribal elements scattered among the huts",
+    "a cave with glowing cracks showing through the immense walls");
 
   private RoomFactory() {}
 
