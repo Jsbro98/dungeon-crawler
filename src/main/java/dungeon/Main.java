3045ceb94ef59955ffceb4e81178a835de334a67
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-08 20:08:59 -0600

    updated test case

--- src/main/java/dungeon/Main.java
+++ src/main/java/dungeon/Main.java
@@ -1,14 +1,15 @@
 package dungeon;
 
 import dungeon.logic.Game;
+import dungeon.logic.RoomFactory;
 import dungeon.world.Room;
 
 public class Main {
   static void main(String[] args) {
     Game gameLoop = new Game(new Player(100, 10));
 
-    Room roomOne = new Room("a cold tundra with no trees for miles");
-    Room roomTwo = new Room("a dense forest with trees everywhere. creatures lurk in the fog...");
+    Room roomOne = RoomFactory.createRoomWithDescription();
+    Room roomTwo = RoomFactory.createRoomWithDescription();
     roomOne.setExit("north", roomTwo);
     roomTwo.setExit("south", roomOne);
 
