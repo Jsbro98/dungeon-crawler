cab8a12fecbb15c77ec875e3caa9867f6a8b1749
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-11 19:54:56 -0600

    added missing inventory case

--- src/main/java/dungeon/logic/CommandDispatcher.java
+++ src/main/java/dungeon/logic/CommandDispatcher.java
@@ -11,6 +11,7 @@ public class CommandDispatcher {
       case "exit" -> new ParsedCommand(Command.EXIT, null);
       case "directions" -> new ParsedCommand(Command.DIRECTIONS, null);
       case "attack" -> new ParsedCommand(Command.ATTACK, null);
+      case "inventory" -> new ParsedCommand(Command.INVENTORY, null);
       case "go" -> new ParsedCommand(Command.MOVE, arg);
       case "pickup" -> new ParsedCommand(Command.PICKUP, arg);
       case "equip" -> new ParsedCommand(Command.EQUIP, arg);
