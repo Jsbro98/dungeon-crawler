9a88deee81fe0f6cc37ec0281bc088521eeadf20
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-13 19:49:15 -0600

    fixed and shortened ItemValidator

--- src/main/java/dungeon/logic/ItemValidator.java
+++ src/main/java/dungeon/logic/ItemValidator.java
@@ -3,36 +3,21 @@ package dungeon.logic;
 import dungeon.Item;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class ItemValidator {
-  public Item validateItem(String itemName) {
+  private static final Map<String, Item> ITEM_REGISTRY =
+          Arrays.stream(Item.values())
+                  .filter(item -> !item.isNothing())
+                  .collect(Collectors.toMap(
+                          item -> item.getName().toLowerCase(),
+                          item -> item
+                  ));
+
+  public Optional<Item> validateItem(String itemName) {
     itemName = itemName.trim().toLowerCase();
-    // TODO: continue here. Have itemName switch on all types
-    //  from the Item enum, check the name, then return the Item
-  }
-
-  private Item.ItemType getTypeOfItem(String name) {
-    List<String> parts = Arrays.stream(name.split(" ")).toList();
-    if (parts.contains("potion")) {
-      return Item.ItemType.POTION;
-    }
-
-    if (isStringAnItem(name)) {
-      return Item.ItemType.WEAPON;
-    }
-
-    return Item.ItemType.BLANK;
-  }
-
-  private boolean isStringAnItem(String arg) {
-    for (Item item : Item.values()) {
-      String itemName = item.getName().toLowerCase();
-      if (arg.equals(itemName)) {
-        return true;
-      }
-    }
-
-    return false;
+    return Optional.ofNullable(ITEM_REGISTRY.get(itemName));
   }
 }
