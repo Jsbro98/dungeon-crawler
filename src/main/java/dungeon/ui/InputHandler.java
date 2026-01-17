e223a6f17f8bd4c8fb7bc9c9d88802e3d10a6976
Jacob Harwood <96179296+Jsbro98@users.noreply.github.com>
2026-01-16 19:19:02 -0600

    added yes/no method for InputHandler

--- src/main/java/dungeon/ui/InputHandler.java
+++ src/main/java/dungeon/ui/InputHandler.java
@@ -9,4 +9,16 @@ public class InputHandler {
     IO.print("> ");
     return userIn.nextLine().trim().toLowerCase();
   }
+
+  public boolean getYesNo() {
+    IO.print("Would you like to continue with this action (y/n): ");
+    while (true) {
+      String input = userIn.nextLine().trim();
+
+      if (input.equalsIgnoreCase("y")) return true;
+      if (input.equalsIgnoreCase("n")) return false;
+
+      IO.println("Please enter 'y' for yes, 'n' for no");
+    }
+  }
 }
